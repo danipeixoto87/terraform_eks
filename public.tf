2ccbdd9c9e82d56afb61e1174945324ccb9012c6
root <root@LAPTOP-0D5BHIE7.localdomain>
2026-09-18 17:24:40 -0300

    feat: Add private subnets

--- public.tf
+++ public.tf
@@ -1,21 +1,29 @@
 resource "aws_subnet" "eks-public-1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "Subnet Pública 1A"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "devops-public-1a"
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
 }
 
 resource "aws_subnet" "eks-public-1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "Subnet Pública 1B"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "devops-public-1b"
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
 }
